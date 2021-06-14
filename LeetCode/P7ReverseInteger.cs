f55f31c713b4c847544955cdcd22f1e7b734e84a
Abhinav Galodha <abhinav9galodha@gmail.com>
2021-06-13 23:37:52 -0500

    ReverseString - Modified Code to Update the quotient.

--- LeetCode/P7ReverseInteger.cs
+++ LeetCode/P7ReverseInteger.cs
@@ -6,27 +6,61 @@ Example 1:
 
 Input: x = 123
 Output: 321
+
+
+Algo
+//1. Get the length of the string = len 
+//2. Set multiplication factor = 10 exp len
+//3. while the divisor is not zero
+//4. Get the remainder = 3
+//5. Multiply remainder by the multiplication factor from step2
+//6. add to the result
+//7. set the remaining number = number\10
+//8.
+
+Dry Run
+1. 2                                
+2. 100
+3. 123/10 = 12 | false
+4. 123%10 = 3
+5. 3 * 100 = 300
+6. 0 + 300 = 300
+7. number = 12
+
+3.
+12/10 = 1 | false
+12%10 = 2
+2*10 = 20
+300 + 20 = 320
+number = 1
+
 */
+
+
 
 using System;
 namespace LeetCode
 {
     public class P7ReverseInteger
     {
-        public int Reverse(int x) {
-        
-        var result = 1;
-        var multiplicationFactor = Math.Pow(10, x.ToString().Length);
-        var remainingNumber = x;
-        while(remainingNumber/10 == 0)
+        public int Reverse(int x)
         {
-           result = (int) (result * multiplicationFactor) + x % 10;
-           Console.WriteLine(result);
-           multiplicationFactor = multiplicationFactor/10;
-           remainingNumber = remainingNumber/10;   
+            var result = 0;
+            var multiplicationFactor = Math.Pow(10, x.ToString().Length);
+
+            var remainingNumber = x;
+            var remainder = 0;
+            var quotient = x;
+
+            while (quotient != 0)
+            {
+                remainder = quotient % 10;
+                quotient = x / 10;
+                result = (int)(result + (remainder * multiplicationFactor));
+                multiplicationFactor = multiplicationFactor / 10;
+            }
+            return result;
         }
-        return result;
-    }
     }
 }
 
