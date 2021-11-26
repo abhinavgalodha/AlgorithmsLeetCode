36ae42e33bff7a383f66663e076e6f6c8e3ddbc3
Abhinav Galodha <abhinav9galodha@gmail.com>
2021-11-25 23:11:57 -0600

    DP - Counting Bits - Code and Test added

---
+++ LeetCode.Test/P338CountingBitsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+using LeetCode.Test.Extension;
+
+namespace LeetCode.Test
+{
+    public class P338CountingBitsTests
+    {
+        public static IEnumerable<object[]> TestData =>
+            new List<object[]>
+            {
+                new object[]{ 2 , new int[] {0,1,1} },
+                new object[]{ -123 ,new int[] {0,1,1,2,1,2} },
+            };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void PositiveTests(int inputNum, int[] expectedArray)
+        {
+            var output = new P338CountingBits().CountBits(inputNum);
+            output.Should().BeEquivalentWithStrictOrderTo(expectedArray);
+        }
+    }
+}
---
+++ LeetCode/P338CountingBits.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /*
+     * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+        Example 1:
+        
+        Input: n = 2
+        Output: [0,1,1]
+        Explanation:
+        0 --> 0
+        1 --> 1
+        2 --> 10
+    */
+    public class P338CountingBits
+    {
+        public int[] CountBits(int n)
+        {
+
+            if (n <= 0)
+                return new int[0];
+
+            // Declare an output array whose length is n+1
+            var answerArray = new int[n + 1];
+
+            for (var i = 0; i <= n + 1; i++)
+            {
+                var countBits = Convert.ToString(i, 2).Count(x => x == '1');
+                answerArray[i] = countBits;
+            }
+
+            // ans[i] = Count of 1's
+            // ans [i] =  get binary representation of a number
+
+            return answerArray;
+        }
+    }
+}
